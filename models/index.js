b1ac59f1daf51178e21b71340245e26902e42c5b
Jannimal804 <jannalhall@gmail.com>
2019-05-01 16:02:07 -0400

    errors fixed

--- models/index.js
+++ models/index.js
@@ -7,26 +7,47 @@ const Sequelize = require('sequelize');
 const basename = path.basename(module.filename);
 const env = process.env.NODE_ENV || 'development';
 const config = require(__dirname + '/../config/config.json')[env];
-const db = {};
-
-if (process.env.JAWSDB_URL) {
-  connection = mysql.createConnection(process.env.JAWSDB_URL);
-} else {
-  connection = mysql.createConnection
-({
-  host: 'localhost',
-  user: 'root',
-  password: 'Gobby911!',
-  database: 'reactbb'
-});
+const db = {
+    // localhost
+    localhost: {
+      host: "localhost",
+      port: 3306,
+      user: "root",
+      password: "Gobby911!",
+      database: "reactbb"
+    },
+  
+    // jawsDB
+    jawsDB: {
+      host: "r4919aobtbi97j46.cbetxkdyhwsb.us-east-1.rds.amazonaws.com",
+      port: 3306,
+      user: "tu8b11r97wchwahe",
+      password: "yuiipv4vzv2mimh5",
+      database: "zs0bi1zchir55u2a"
+    }
+  
 };
+// we placed the connections in this source object
+
+
+
+// we use source.[name of connection] to hook into mysql
+// var connection = mysql.createConnection(source.localhost);
+
+// connection.connect(function(err) {
+//   if (err) {
+//     console.error("error connecting: " + err.stack);
+//     return;
+//   }
+//   console.log("connected as id " + connection.threadId);
+// });
 
 //let sequelize;
-// if (config.use_env_variable) {
-//   var sequelize = new Sequelize(process.env[config.use_env_variable]);
-// } else {
-//   var sequelize = new Sequelize(config.database, config.username, config.password, config);
-// }
+if (config.use_env_variable) {
+  var sequelize = new Sequelize(process.env[config.use_env_variable]);
+} else {
+  var sequelize = new Sequelize(config.database, config.username, config.password, config);
+}
 
 fs
   .readdirSync(__dirname)
--- server.js
+++ server.js
@@ -1,16 +1,18 @@
 const express = require("express");
 const path = require("path");
 const PORT = process.env.PORT || 3001;
+var cors = require("cors");
 const app = express();
 var db = require("./models");
 //var passport = require("passport");
 
-app.listen(process.env.PORT || 3000, function(){
-    console.log("Express server listening on port", this.address().port, app.settings.env);
-  });
+// app.listen(process.env.PORT || 3000, function(){
+//     console.log("Express server listening on port", this.address().port, app.settings.env);
+//   });
 
 // Define middleware here
 app.use(express.urlencoded({ extended: true }));
+app.use(cors());
 app.use(express.json());
 
 require("./routes/api-routes")(app);
@@ -19,6 +21,13 @@ require("./routes/api-routes")(app);
 // app.get("*", (req, res) => {
 //    res.sendFile(path.join(__dirname + "/build/index.html"));
 // });
+if(process.env.NODE_ENV === 'production'){
+    app.use(express.static('client/build'));
+   };
+
+app.get("/", (req, res) => {
+    res.sendFile(path.join(__dirname, "./client/public/index.html"));
+  });
 
 // db.sequelize.sync({ force: true}).then(function(){
    db.sequelize.sync().then(function(){
